13c6718285d397b5b460b6b51c886350e023d6d9
Lugi Cuk Nahgud <47616503+lugibyte@users.noreply.github.com>
2020-01-14 13:53:20 +0700

    Add files via upload

---
+++ Encapitulation/src/pt2/MyClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt2;
+
+/**
+ *
+ * @author windows
+ */
+    public class MyClass {
+  public static void main(String[ ] args) {
+    Person myObj = new Person();
+    myObj.setName("John");
+    System.out.println(myObj.getName());
+  }
+}
+
+
---
+++ Encapitulation/src/pt2/Person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt2;
+
+/**
+ *
+ * @author windows
+ */
+
+//Person.java :
+
+public class Person {
+   private String name;
+
+   // Getter
+   public String getName() {
+     return name;
+   }
+
+   // Setter
+   public void setName(String newName) {
+     this.name = newName;
+   }
+
+}
